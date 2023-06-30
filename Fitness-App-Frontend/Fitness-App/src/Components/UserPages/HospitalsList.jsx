5cc010d56e1641f253a2037172fc8b1f7e793c77
Rakesh Mandal <rkm8448746347@gmail.com>
2023-06-30 23:40:59 +0530

    last commit of today

--- Fitness-App-Frontend/Fitness-App/src/Components/UserPages/HospitalsList.jsx
+++ Fitness-App-Frontend/Fitness-App/src/Components/UserPages/HospitalsList.jsx
@@ -1,9 +1,12 @@
 import { useSelector,useDispatch } from "react-redux";
 import { getHospitals } from "../API-calls/hospitalApi";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import HospitalCard from "./hospitalCard";
+import { Modal } from "react-bootstrap";
 
 export default function HospitalList() {
+  let [showModal, setShowModal] = useState(false);
+  let [hospitalId,setHospitalId]=useState("")
       
   let HospitalList = useSelector(state => state.HospitalList);
 
@@ -12,7 +15,13 @@ export default function HospitalList() {
   useEffect(() => {
     fetchAllHospitals();
   },[])
-  console.log(HospitalList)
+  
+  let openModal = (id)=>{
+    setHospitalId(id)
+    setShowModal(true)
+  }
+  
+  console.log(hospitalId)
   
   let fetchAllHospitals = async () => {
     
@@ -30,21 +39,75 @@ export default function HospitalList() {
 
   }
     
-  return(
+  return (
     <div className="d-flex justify-content-center">
-      <div style={{flex:"0.3"}}>
-      </div> 
-      <div style={{backgroundColor:"lightgrey"}} className=" shadow-lg ">
+      <div style={{ flex: "0.3" }}></div>
+      <div style={{ backgroundColor: "lightgrey" }} className="w-50 shadow-lg ">
         {HospitalList.map((hospital) => {
-          return <div   key={hospital._id}>
-             <HospitalCard/>
+          return (
+            <div key={hospital._id}>
+              <HospitalCard hospital={hospital} openModal={openModal} />
             </div>
+          );
         })}
-
       </div>
-      <div style={{ flex:"0.2"}}></div>
-  </div>
-  )
+      {showModal && (
+        <div>
+          <Modal
+            show={showModal}
+            onHide={() => setShowModal(false)}
+            fullscreen={true}
+            centered
+            backdrop="static"
+          >
+            <Modal.Header  closeButton>Appointment Booking Form</Modal.Header>
+            <Modal.Body>
+              <form>
+                <div>
+                  <label>Hospital </label>
+                  <input />
+                </div>
+                <div>
+                  <label>Department </label>
+                  <select value="select">
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                  </select>
+                </div>
+                <div>
+                  <label> Symptoms's Category </label>
+                  <select>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                  </select>
+                </div>
+                <div>
+                  <label>Duration/Timings </label>
+                  <input />
+                </div>
+                <div>
+                  <label> short descripion on how you feels </label>
+                  <input />
+                </div>
+                <div>
+                  <label>Appointment Date</label>
+                  <input type="datetime-local" />
+                </div>
+              </form>
+            </Modal.Body>
+          </Modal>
+        </div>
+      )}
+      <div style={{ flex: "0.3" }}></div>
+    </div>
+  );
 }
 
 
--- Fitness-App-Frontend/Fitness-App/src/Components/UserPages/hospitalCard.jsx
+++ Fitness-App-Frontend/Fitness-App/src/Components/UserPages/hospitalCard.jsx
@@ -1,25 +1,26 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-function HospitalCard() {
+function HospitalCard({ hospital,openModal }) {
+  
     return (
 
         <div className="m-3 p-2 hospitalCard">
-      <Card style={{ width: "100%", height: "46vh" }}>
+      <Card style={{ width: "100%", height: "49vh" }}>
         <Card.Img
           variant="top"
           src="https://thumbs.dreamstime.com/b/hospital-building-modern-parking-lot-59693686.jpg"
           style={{ width: "100%", height: "12em" }}
         />
         <Card.Body className="bg">
-          <Card.Title>Hospital Name</Card.Title>
+            <Card.Title>{hospital.hospitalName}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+              {hospital.hospitalAddress}
+                <Card.Text>description of what all kinds of disease being cured here </Card.Text>
           </Card.Text>
-          <div className="d-flex justify-content-between">
+          <div >
             {" "}
-            <Button variant="primary">Book Appointment</Button>
+            <Button onClick={()=>openModal(hospital._id)} className="mx-2" variant="primary">Book Appointment</Button>
             <Button variant="success"> Know More ...</Button>
           </div>
         </Card.Body>
