b397a2c96e7fd2d74becf48e47acdf3412c33150
Rakesh Mandal <87007801+Rakesh-08@users.noreply.github.com>
2023-12-02 23:41:14 +0530

    Update authController.js

--- Fitness-App-Backend/controllers/authController.js
+++ Fitness-App-Backend/controllers/authController.js
@@ -10,6 +10,16 @@ const authConfig = require("../configs/authConfig");
 let signUp = async (req, res) => {
 
     try {
+  let totalUsers = await userModel.find({});
+
+        if (totalUsers.length > 15) {
+            return res.status(300).send({
+                message:"Sorry! this application has limited capacity and its reached its maximum"
+            })
+        }
+
+        // send email to the admin that somebody has signed up ;
+         
     
         let user = await userModel.create({
             firstName: req.body.firstName,
